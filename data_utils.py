bbf973f2db6226ff014bf0bddfab6a56f47ff19c
EfratHoresh <efratporten@mail.tau.ac.il>
2023-08-14 15:10:23 +0300

    another try with prepared dataset

--- data_utils.py
+++ data_utils.py
@@ -7,62 +7,70 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import json
 
 
-#      ------------------------ Sentiment Analysis  -------------------------------- #
+# #      ------------------------ Sentiment Analysis  -------------------------------- #
 
-# load tweets-2020-2021-subset dataset
-def load_json(path):
-    with open(path) as f:
-        return [json.loads(l.strip()) for l in f]
+# # load tweets-2020-2021-subset dataset
+# def load_json(path):
+#     with open(path) as f:
+#         return [json.loads(l.strip()) for l in f]
 
-def create_text_list(dataset_list):
-    text_list = ['' for i in range(len(dataset_list))]
-    for i in range(len(dataset_list)):
-        text_list[i] = dataset_list[i]['text']
-    return text_list
+# def create_text_list(dataset_list):
+#     text_list = ['' for i in range(len(dataset_list))]
+#     for i in range(len(dataset_list)):
+#         text_list[i] = dataset_list[i]['text']
+#     return text_list
 
-def create_text_data():
-    dataset_list = load_json("tweets-2020-2021-subset-rnd.jl")
-    text_list = create_text_list(dataset_list)
-    sentiment_file = open("text_data.txt", "a")
-    for text in text_list:
-        sentiment_file.write(text + "\n")
-    return text_list
+# def create_text_data():
+#     dataset_list = load_json("tweets-2020-2021-subset-rnd.jl")
+#     text_list = create_text_list(dataset_list)
+#     sentiment_file = open("text_data.txt", "a")
+#     for text in text_list:
+#         sentiment_file.write(text + "\n")
+#     return text_list
 
 
-# Preprocess text (username and link placeholders)
-def preprocess(text):
-    new_text = []
-    for t in text.split(" "):
-        t = '@user' if t.startswith('@') and len(t) > 1 else t
-        t = 'http' if t.startswith('http') else t
-        new_text.append(t)
-    return " ".join(new_text)
+# # Preprocess text (username and link placeholders)
+# def preprocess(text):
+#     new_text = []
+#     for t in text.split(" "):
+#         t = '@user' if t.startswith('@') and len(t) > 1 else t
+#         t = 'http' if t.startswith('http') else t
+#         new_text.append(t)
+#     return " ".join(new_text)
 
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
-# tokenizer = AutoTokenizer.from_pretrained(MODEL)
-# config = AutoConfig.from_pretrained(MODEL)
-# # PT
-# model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-# text = "Covid cases are increasing fast!"
-# text = preprocess(text)
-# encoded_input = tokenizer(text, return_tensors='pt')
-# output = model(**encoded_input)
-# scores = output[0][0].detach().numpy()
-# scores = softmax(scores)
-# ranking = np.argsort(scores)
-# ranking = ranking[::-1]
-# for i in range(scores.shape[0]):
-#     l = config.id2label[ranking[i]]
-#     s = scores[ranking[i]]
-#     print(f"{i+1}) {l} {np.round(float(s), 4)}")
+# MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+# # tokenizer = AutoTokenizer.from_pretrained(MODEL)
+# # config = AutoConfig.from_pretrained(MODEL)
+# # # PT
+# # model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+# # text = "Covid cases are increasing fast!"
+# # text = preprocess(text)
+# # encoded_input = tokenizer(text, return_tensors='pt')
+# # output = model(**encoded_input)
+# # scores = output[0][0].detach().numpy()
+# # scores = softmax(scores)
+# # ranking = np.argsort(scores)
+# # ranking = ranking[::-1]
+# # for i in range(scores.shape[0]):
+# #     l = config.id2label[ranking[i]]
+# #     s = scores[ranking[i]]
+# #     print(f"{i+1}) {l} {np.round(float(s), 4)}")
 
-# Pipeline example:
-text_list = create_text_data()
-sentiment_task = pipeline("sentiment-analysis", model=MODEL, tokenizer=MODEL)
-result = sentiment_task("Covid cases are increasing fast!",return_all_scores=True)
+# # Pipeline example:
+# text_list = create_text_data()
+# sentiment_task = pipeline("sentiment-analysis", model=MODEL, tokenizer=MODEL)
+# result = sentiment_task("Covid cases are increasing fast!",return_all_scores=True)
 
-sentiment_file = open("twitter_roberta_sentiment_results.txt","a")
-for text in text_list:
-    result = sentiment_task(text, return_all_scores=True)
-    sentiment_file.write(str(result) + "\n")
-sentiment_file.close()
+# sentiment_file = open("twitter_roberta_sentiment_results.txt","a")
+# for text in text_list:
+#     result = sentiment_task(text, return_all_scores=True)
+#     sentiment_file.write(str(result) + "\n")
+# sentiment_file.close()
+
+
+# ------------------second try
+from datasets import load_dataset
+
+dataset = load_dataset("parquet", data_files='/home/efrath/repos/NLP-final-project/tweet_sentiment_multilingual-train.parquet')
+
+print("!")
